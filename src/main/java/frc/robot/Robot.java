a4e99ac790efc045a60dab37f52d8da28b03ebf3
ryancave282 <ryancave282@gmail.com>
2024-10-28 20:55:22 -0500

    add comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
@@ -11,30 +7,42 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
+  //create motors
   private final CANSparkMax m_frontLeftMotor = new CANSparkMax(3, MotorType.kBrushed);
   private final CANSparkMax m_frontRightMotor = new CANSparkMax(1, MotorType.kBrushed);
   private final CANSparkMax m_backLeftMotor = new CANSparkMax(4, MotorType.kBrushed);
   private final CANSparkMax m_backRightMotor = new CANSparkMax(2, MotorType.kBrushed);
+  
+  //create diff drive with front motors as parent
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeftMotor, m_frontRightMotor);
+  
+  //create controller
   private final CommandXboxController driverController = new CommandXboxController(0);
 
   public Robot() {
+    //set back motors to follow front motors
     m_backLeftMotor.follow(m_frontLeftMotor);
     m_backRightMotor.follow(m_frontRightMotor);
   }
 
   @Override
   public void robotInit() {
+    //set motor inverted status
     m_frontRightMotor.setInverted(true);
     m_backRightMotor.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
+    //slowmode
     double multiplier = 0.5;
+    
+    //set slowmode status with "y" button
     if (driverController.y().getAsBoolean()) {
+      //slowmode drive
       m_robotDrive.arcadeDrive(multiplier * driverController.getLeftY(), multiplier * driverController.getRightX());
     } else {
+      //normal drive
       m_robotDrive.arcadeDrive(driverController.getLeftY(), driverController.getRightX());
     }
    
